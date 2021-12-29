9ae673cc437b069f63638876b39bba35b1d06d82
apustovitin <apustovitin@gmail.com>
2021-12-30 01:32:17 +0300

    fix auth0

--- backend/vinet/videos/apps.py
+++ backend/vinet/videos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Auth0AuthorizationConfig(AppConfig):
+class VideosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth0authorization'
+    name = 'videos'
--- backend/vinet/videos/utils.py
+++ backend/vinet/videos/utils.py
@@ -10,7 +10,7 @@ def jwt_get_username_from_payload_handler(payload):
 
 def jwt_decode_token(token):
     header = jwt.get_unverified_header(token)
-    jwks = requests.get('https://{}/.well-known/jwks.json'.format('YOUR_DOMAIN')).json()
+    jwks = requests.get('https://{}/.well-known/jwks.json'.format('dev-2b971qr7.eu.auth0.com')).json()
     public_key = None
     for jwk in jwks['keys']:
         if jwk['kid'] == header['kid']:
@@ -19,5 +19,5 @@ def jwt_decode_token(token):
     if public_key is None:
         raise Exception('Public key not found.')
 
-    issuer = 'https://{}/'.format('YOUR_DOMAIN')
-    return jwt.decode(token, public_key, audience='YOUR_API_IDENTIFIER', issuer=issuer, algorithms=['RS256'])
+    issuer = 'https://{}/'.format('dev-2b971qr7.eu.auth0.com')
+    return jwt.decode(token, public_key, audience='https://vinet/', issuer=issuer, algorithms=['RS256'])
